dcca4bc6d4b7aef59823adbfa1f18372d4854735
Triibu Nupsik <triibunupsik@gmail.com>
2024-10-24 10:24:57 +0300

    Initial commit

---
+++ src/main/java/me/triibu_mod/Triibu_mod.java
@@ -0,0 +1,10 @@
+package me.triibu_mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Triibu_mod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
