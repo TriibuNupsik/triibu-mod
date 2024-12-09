25d942b0dce3418e62ad7d337e531b9b0d908b94
TriibuNupsik <triibunupsik@gmail.com>
2024-12-09 12:43:17 +0200

    # Added webAPI to access info about the world.
    ### Block API
    
    x,y,z
    http://localhost:8080/world/block?x=1&y=56&z=3
    
    ### Slab API
    
    xStart, yStart, zStart, xEnd, yEnd, zEnd
    http://localhost:8080/world/slab?xStart=0&yStart=-64&zStart=0&xEnd=8&yEnd=600&zEnd=8

--- src/main/java/me/triibu_mod/mixin/TriibuMixin.java
+++ src/main/java/me/triibu_mod/mixin/TriibuMixin.java
@@ -1,5 +1,6 @@
 package me.triibu_mod.mixin;
 
+import me.triibu_mod.WorldAPI;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,8 +9,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class TriibuMixin {
+
+    @Inject(method = "runServer", at = @At("HEAD"))
+    public void onServerStart(CallbackInfo info) {
+        WorldAPI.setMinecraftServer((MinecraftServer) (Object) this);
+    }
+
     @Inject(at = @At("HEAD"), method = "loadWorld")
     private void init(CallbackInfo info) {
-
     }
 }
